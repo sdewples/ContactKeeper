f724398da339631c9e1022ab918dea59542cb062
daniel <sdewples@gmail.com>
2019-11-21 14:20:49 -0500

    finished auth and user routes with jwt and encrypted passwords

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  mongoose
+    .connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true
+    })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => {
+      console.log(err.message);
+      process.exit(1);
+    });
+};
+
+module.exports = connectDB;
